af897162c37b64946d59633cf542dfcf68732bb1
Sabin-23 <123ishimwesabin@gmail.com>
2026-05-23 21:04:25 +0200

    Fixed

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
